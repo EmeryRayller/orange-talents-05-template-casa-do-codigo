d460edf86913349763aeee3d77f65edf46d5c12c
Rayller <rayller.emery@zup.com.br>
2021-05-19 20:07:22 -0300

    terceiro cartao lista
    Criação do método de listar livros.

--- src/main/java/me/rayll/livros/LivroController.java
+++ src/main/java/me/rayll/livros/LivroController.java
@@ -1,10 +1,15 @@
 package me.rayll.livros;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +29,15 @@ public class LivroController {
 		Livro livro = form.toModel(manager);
 		manager.persist(livro);
 		return livro.toDto();
+	}
+	
+	@GetMapping
+	@Transactional
+	public List<LivroDTO> buscarLivros() {
+		Query query = manager.createQuery("select l from Livro l");
+		List<Livro> listaLivros = query.getResultList();
+		List<LivroDTO> dtos = listaLivros.stream().map( livro -> livro.toDto()).collect(Collectors.toList());
+		return dtos;
 	}
 	
 }

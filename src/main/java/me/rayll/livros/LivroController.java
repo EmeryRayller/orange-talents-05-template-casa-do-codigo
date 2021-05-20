32a927fd578f5033f93fa33f7e54341963d2c7ba
Rayller <rayller.emery@zup.com.br>
2021-05-20 09:20:34 -0300

    lista de livros v.1.1

--- src/main/java/me/rayll/livros/LivroController.java
+++ src/main/java/me/rayll/livros/LivroController.java
@@ -9,10 +9,13 @@ import javax.persistence.Query;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -31,13 +34,19 @@ public class LivroController {
 		return livro.toDto();
 	}
 	
+	
 	@GetMapping
 	@Transactional
 	public List<LivroDTO> buscarLivros() {
+
 		Query query = manager.createQuery("select l from Livro l");
 		List<Livro> listaLivros = query.getResultList();
-		List<LivroDTO> dtos = listaLivros.stream().map( livro -> livro.toDto()).collect(Collectors.toList());
+		List<LivroDTO> dtos = listaLivros.stream().map(livro -> livro.toDto()).collect(Collectors.toList());
 		return dtos;
+
 	}
+	
+	
+	
 	
 }
